42187048ee8eb672c61b345c3e91428af7d6ade6
BoHdANchIck <daneliuk.bohdan@chnu.edu.ua>
2025-02-22 13:29:14 +0200

    initial commit

---
+++ Lab_3_Daneliuk/Lab_3_Daneliuk/Program.cs
@@ -0,0 +1,142 @@
+﻿namespace Lab_3_Daneliuk
+{
+    internal class Program
+    {
+        abstract class Test
+        {
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+
+            public Test(string name, DateTime date)
+            {
+                Name = name;
+                Date = date;
+            }
+
+            public abstract void Show(); 
+        }
+
+        class Exam : Test
+        {
+            public string Subject { get; set; }
+            public int Score { get; set; }
+
+            public Exam(string name, DateTime date, string subject, int score)
+                : base(name, date)
+            {
+                Subject = subject;
+                Score = score;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine($"Exam: {Name}, Date: {Date.ToShortDateString()}, Subject: {Subject}, Score: {Score}");
+            }
+        }
+
+        class FinalExam : Exam
+        {
+            public string DifficultyLevel { get; set; }
+
+            public FinalExam(string name, DateTime date, string subject, int score, string difficultyLevel)
+                : base(name, date, subject, score)
+            {
+                DifficultyLevel = difficultyLevel;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine($"Final Exam: {Name}, Date: {Date.ToShortDateString()}, Subject: {Subject}, Score: {Score}, Difficulty: {DifficultyLevel}");
+            }
+        }
+
+        class Trial : Test
+        {
+            public string TrialType { get; set; }
+
+            public Trial(string name, DateTime date, string trialType)
+                : base(name, date)
+            {
+                TrialType = trialType;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine($"Trial: {Name}, Date: {Date.ToShortDateString()}, Type: {TrialType}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task1();
+            Console.WriteLine();
+            Console.ReadKey();
+            Task2();
+            Console.ReadKey();
+        }
+
+        static void Task1() 
+        {
+            Console.WriteLine("#Task_1");
+            Random random = new Random();
+            int n;
+            Console.Write("Enter the number of trapezes: ");
+            n = int.Parse(Console.ReadLine());
+
+            Trapeze[] trapezes = new Trapeze[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = random.Next(1, 20);
+                int b = random.Next(1, 20);
+                int h = random.Next(1, 20);
+                int color = random.Next(1, 256);
+                trapezes[i] = new Trapeze(a, b, h, color);
+            }
+
+            Console.WriteLine("\nTrapezes Information:");
+            int squareCount = 0;
+            foreach (var trap in trapezes)
+            {
+                trap.PrintInfo();
+                Console.WriteLine($"Area: {trap.Area():F2}");
+                Console.WriteLine($"Perimeter: {trap.Perimeter():F2}");
+                Console.WriteLine(trap.IsSquare ? "This trapeze is a square!" : "This trapeze is not a square.");
+                Console.WriteLine();
+
+                if (trap.IsSquare)
+                {
+                    squareCount++;
+                }
+            }
+
+            Console.WriteLine($"Total number of squares: {squareCount}");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("#Task_2");
+            Test[] tests = new Test[]
+            {
+                new Exam("Math Exam", new DateTime(2025, 5, 20), "Math", 85),
+                new FinalExam("Graduation Exam", new DateTime(2025, 6, 10), "Physics", 90, "High"),
+                new Trial("Driving Test", new DateTime(2025, 3, 15), "Practical"),
+                new Exam("History Exam", new DateTime(2025, 5, 18), "History", 78),
+                new FinalExam("Final Chemistry", new DateTime(2025, 6, 5), "Chemistry", 88, "Medium")
+            };
+
+            Console.WriteLine("All Tests:");
+            foreach (var test in tests)
+            {
+                test.Show();
+            }
+
+            Array.Sort(tests, (t1, t2) => t1.Date.CompareTo(t2.Date));
+
+            Console.WriteLine("\nSorted by Date:");
+            foreach (var test in tests)
+            {
+                test.Show();
+            }
+        }
+    }
+}
---
+++ Lab_3_Daneliuk/Lab_3_Daneliuk/Trapeze.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_Daneliuk
+{
+    class Trapeze
+    {
+        private int a, b, h; 
+        private int c;       
+
+        public Trapeze(int a, int b, int h, int color)
+        {
+            this.a = a;
+            this.b = b;
+            this.h = h;
+            this.c = color;
+        }
+
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public int B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public int H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public int Color
+        {
+            get { return c; }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Trapeze: a = {a}, b = {b}, h = {h}, color = {c}");
+        }
+
+        public double Perimeter()
+        {
+            double side = Math.Sqrt(Math.Pow((a - b) / 2.0, 2) + Math.Pow(h, 2));
+            return a + b + 2 * side;
+        }
+
+        public double Area()
+        {
+            return ((a + b) / 2.0) * h;
+        }
+
+        public bool IsSquare
+        {
+            get { return a == b && a == h; }
+        }
+    }
+}
+
